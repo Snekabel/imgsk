ca88c8c5508c90feba878f9ca0d4c56e4e4790a4
TBthegr81 <tbthegr81@gmail.com>
2019-01-26 01:23:42 +0100

    Added upload form if no file is being uploaded

---
+++ uploadForm.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+</head>
+<body>
+    <form enctype="multipart/form-data" method="POST" action="index.php">
+      <input type="file" name="upfile"/>
+      <button>Upload</button>
+    </form>
+</body>
+</html>
